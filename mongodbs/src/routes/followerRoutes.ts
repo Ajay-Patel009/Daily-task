34a8ad29af6a06ac945e93e770f0705da35c17c7
Ajay-Patel009 <ajay.patel@appinventiv.com>
2023-08-03 11:02:24 +0530

    changes

--- mongodbs/src/routes/followerRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { follower } from '../controllers/getFollowersController';
-
-
-const router = express.Router();
-
-router.post('/',follower);
-
-export default router;
